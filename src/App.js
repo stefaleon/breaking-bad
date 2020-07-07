74d2f1426bc43e6a355b083dc1d09a717e71d574
stefaleon <stefaleon@gmail.com>
2020-07-07 18:53:49 +0300

    characters grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './components/UI/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
 
 function App() {
-  return <div className='container'>ohai!</div>;
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true);
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+        // `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div className='container'>
+      <Header />
+      <CharacterGrid isLoading={isLoading} items={items} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CharacterGrid = ({ items, isLoading }) => {
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className='cards'>
+      {items.map((item) => (
+        <h1>{item.name}</h1>
+      ))}
+    </section>
+  );
+};
+
+CharacterGrid.propTypes = {};
+
+export default CharacterGrid;
